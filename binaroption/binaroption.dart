c99e7dc08fc346cc5dc0d052220eead9bc9fcebb
kyrgyz00 <bekbolottashpolotov@gmail.com>
2022-10-28 19:03:05 +0600

    first commit

---
+++ binaroption/binaroption.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main() {
+  
+  binar_1();
+}
+
+
+binar_1() {
+  print("imagine a number from 1 till 128,be honest");
+  int min = 0;
+  int max = 128;
+  int mid = (max + min) ~/ 2;
+  stdout.write(" + or - $mid?");
+  String answer = "";
+  bool stopFunc = true;
+  int counter = 0;
+
+  while (answer != "yes") {
+    stdout.write("is it $mid");
+    answer = stdin.readLineSync()!;
+    if (answer == '+') {
+      min = mid;
+      mid = (max + min) ~/ 2;
+    } else if (answer == "-") {
+      max = mid;
+      mid = (max + min) ~/ 2;
+    } else if (answer == "yes") {
+      print("the end of the game");
+    } else {
+      print("the $answer is not correct");
+      answer = stdin.readLineSync()!;
+    }
+    counter++;
+  }
+  print("all is $counter");
+}
